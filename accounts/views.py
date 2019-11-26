a537674276c8f1cfbf585e70ab407efe18af3785
Juan David Torres <juandatorr_1999@hotmail.com>
2019-11-26 00:21:10 -0600

    urls changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+
 from django.shortcuts import render
 from .models import *
 
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+def home(request):
+    return render(request,'registration/login.html')
+
+def register(request):
+    return render(request,'signup.html')
