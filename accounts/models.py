52dd5fbcc445a32a54393a6a1f5678929f7d7236
Juan David Torres <juandatorr_1999@hotmail.com>
2019-11-25 21:43:06 -0600

    models

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Estado(models.Model):
     nombre = models.CharField(max_length = 50)
-    
+
 class Person(models.Model):
     edad = models.IntegerField()
     empresa = models.CharField(max_length=50)
     estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
-
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True, related_name='info')
 
 
 class Evento(models.Model):
