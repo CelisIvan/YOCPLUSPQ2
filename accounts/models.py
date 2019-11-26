f0547ee382c906f2faf02bb4ec3e58fc2e426252
Juan David Torres <juandatorr_1999@hotmail.com>
2019-11-25 20:29:21 -0600

    Creating models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    edad = models.IntegerField()
+    empresa = models.CharField(max_length=50)
+
+
+class Boleto(models.Model):
+    codigo = models.CharField(max_length=50)
+
+
+
+class Evento(models.Model):
+    nombre = models.CharField(max_length=50)
+    costo = models.FloatField()
+
+    empresa = models.CharField(max_length=50)
