7bd95dfc7d147f0800430c9016b0bdb0d198cf4f
CelisIvan <celistek92@gmail.com>
2019-11-29 15:52:57 -0600

    vistas

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='accounts-name'),
     path('register/', views.register, name='accounts-register'),
-    
-    
+
+    path('eventos/',views.eventos),
+
+    path('register_event/',views.register_event),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,16 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect("/home")    
+        return redirect("/home")
     else:
         form = RegisterForm()
     return render(request,'signup.html',{"form":form})
+
+
+def eventos(request):
+    if request.method  == "GET":
+        return render(request,"eventos.html")
+
+def register_event(request):
+    if request.method == "GET":
+        return render(request,"register_event.html")
